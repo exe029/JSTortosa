ba8a811b0865ce5627a3eb6f324e0bcfc7890936
exe029 <exe029@hotmail.com>
2022-06-10 18:19:21 -0300

    des7

--- js/index.js
+++ js/index.js
@@ -44,42 +44,39 @@ const deleteTaskById = (e) =>{
             tasksContainer.removeChild(el)
         }
     })
-
+};
+const setTaskState = async (e) => {
+    const id = e.target.parentNode.value;
+    const result = await tareas.find( tarea => tarea.id == id);//el tiempo de ejecución era tan rápido que a veces result tenía undefined en la línea 51
+    result.state = !result.state;
+    console.log(e.target.parentNode);
+    e.target.parentNode.style.backgroundColor= result.state ? "#10b981" : "#E5E7EB";
+    e.target.style.fill = result.state ? "#FFFFFF" : "#9CA3AF";
+    localStorage.setItem('tareas',JSON.stringify(tareas))
 }
 
 const printTasks = (newTarea) => {
-    if (date.value != "" && type.value != "" && description.value != ""){
-    newTaskCard = document.createElement('li');
+
+    const { type, date, id, description, state } = newTarea;
+
+    const newTaskCard = document.createElement('li');
     newTaskCard.classList.add('card-container'); //<li class="card-container"></li>
     newTaskCard.innerHTML = `
-        <span class="card__type">${newTarea.type}</span>
-        <span class="card__date">${newTarea.date}</span>
-        <h3>${newTarea.description}</h3>
-        <button class="button-delete" value="${newTarea.id}"></button>
-        <button class="check-button">
-            <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" fill="#9CA3AF"><path xmlns="http://www.w3.org/2000/svg" d="M10.6 16.6 17.65 9.55 16.25 8.15 10.6 13.8 7.75 10.95 6.35 12.35ZM12 22Q9.925 22 8.1 21.212Q6.275 20.425 4.925 19.075Q3.575 17.725 2.788 15.9Q2 14.075 2 12Q2 9.925 2.788 8.1Q3.575 6.275 4.925 4.925Q6.275 3.575 8.1 2.787Q9.925 2 12 2Q14.075 2 15.9 2.787Q17.725 3.575 19.075 4.925Q20.425 6.275 21.212 8.1Q22 9.925 22 12Q22 14.075 21.212 15.9Q20.425 17.725 19.075 19.075Q17.725 20.425 15.9 21.212Q14.075 22 12 22ZM12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12ZM12 20Q15.325 20 17.663 17.663Q20 15.325 20 12Q20 8.675 17.663 6.337Q15.325 4 12 4Q8.675 4 6.338 6.337Q4 8.675 4 12Q4 15.325 6.338 17.663Q8.675 20 12 20Z"/></svg>
+        <span class="card__type">${type}</span>
+        <span class="card__date">${date}</span>
+        <h3>${description}</h3>
+        <button class="button-delete" value="${id}"></button>
+        <button class="check-button ${ state ? "check-button--success": "check-button--disabled"}" value="${id}">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" fill="${ state ? "#FFFFFF" :"#9CA3AF" }"><path xmlns="http://www.w3.org/2000/svg" d="M10.6 16.6 17.65 9.55 16.25 8.15 10.6 13.8 7.75 10.95 6.35 12.35ZM12 22Q9.925 22 8.1 21.212Q6.275 20.425 4.925 19.075Q3.575 17.725 2.788 15.9Q2 14.075 2 12Q2 9.925 2.788 8.1Q3.575 6.275 4.925 4.925Q6.275 3.575 8.1 2.787Q9.925 2 12 2Q14.075 2 15.9 2.787Q17.725 3.575 19.075 4.925Q20.425 6.275 21.212 8.1Q22 9.925 22 12Q22 14.075 21.212 15.9Q20.425 17.725 19.075 19.075Q17.725 20.425 15.9 21.212Q14.075 22 12 22ZM12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12ZM12 20Q15.325 20 17.663 17.663Q20 15.325 20 12Q20 8.675 17.663 6.337Q15.325 4 12 4Q8.675 4 6.338 6.337Q4 8.675 4 12Q4 15.325 6.338 17.663Q8.675 20 12 20Z"/></svg>
         </button>
     `
     newTaskCard.children[3].addEventListener('click', deleteTaskById);
-    console.log(newTaskCard.children[3]);
+    newTaskCard.children[4].addEventListener('click', setTaskState);
+    console.log(newTaskCard.children[4]);
     
 
     tasksContainer.appendChild(newTaskCard);
     vaciar.style.display = "none";
-} else{
-    Toastify({
-
-        text: "La tarea no puede estar vacía",
-        
-        duration: 1000,
-        position: "center", // `left`, `center` or `right`
-        style: {
-            background: "linear-gradient(to right, #00b09b, #96c93d)",
-        },
-        
-        }).showToast();
-
-}
 }
 
 // menuNav toggle
@@ -112,12 +109,22 @@ type.addEventListener('change', (e)=>{
 
 addButton.addEventListener('click', (e)=>{
     e.preventDefault();//evita que la pantalla se refresque
-    newTarea.id = tareas.length + 1;
-    tareas.push({...newTarea}); //spread operator para clonar objetos
-    localStorage.setItem('tareas',JSON.stringify(tareas)); //de objeto literal a JSON
-    printTasks(newTarea);
-    // onmousemove();
-    clearFields();
+    if (date.value != "" && type.value != "" && description.value != ""){
+        newTarea.id = tareas.length + 1;
+        tareas.push({...newTarea}); //spread operator para clonar objetos
+        localStorage.setItem('tareas',JSON.stringify(tareas)); //de objeto literal a JSON
+        printTasks(newTarea);
+        clearFields();
+    }else{
+        Toastify({
+            text: "La tarea no puede estar vacía",
+            duration: 1000,
+            position: "center", // `left`, `center` or `right`
+            style: {background: "linear-gradient(to right, #00b09b, #96c93d)",},
+            }).showToast();
+        console.log('En el else');
+    }
+    
 });
 
 
