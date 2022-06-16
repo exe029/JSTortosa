47131c403d50b75cd2800f12523ecbf67f0123c3
exe029 <exe029@hotmail.com>
2022-06-16 14:07:50 -0300

    desfinal

--- js/index.js
+++ js/index.js
@@ -40,7 +40,7 @@ const deleteTaskById = (e) =>{
     const nodeTareas = document.querySelectorAll('.card-container');
     nodeTareas.forEach( el =>{
         console.log(el);
-        if(el.children[3].value == id){
+        if(el.children[3].children[0].value == id){
             tasksContainer.removeChild(el)
         }
     })
@@ -65,14 +65,15 @@ const printTasks = (newTarea) => {
         <span class="card__type">${type}</span>
         <span class="card__date">${date}</span>
         <h3>${description}</h3>
-        <button class="button-delete" value="${id}"></button>
-        <button class="check-button ${ state ? "check-button--success": "check-button--disabled"}" value="${id}">
-            <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" fill="${ state ? "#FFFFFF" :"#9CA3AF" }"><path xmlns="http://www.w3.org/2000/svg" d="M10.6 16.6 17.65 9.55 16.25 8.15 10.6 13.8 7.75 10.95 6.35 12.35ZM12 22Q9.925 22 8.1 21.212Q6.275 20.425 4.925 19.075Q3.575 17.725 2.788 15.9Q2 14.075 2 12Q2 9.925 2.788 8.1Q3.575 6.275 4.925 4.925Q6.275 3.575 8.1 2.787Q9.925 2 12 2Q14.075 2 15.9 2.787Q17.725 3.575 19.075 4.925Q20.425 6.275 21.212 8.1Q22 9.925 22 12Q22 14.075 21.212 15.9Q20.425 17.725 19.075 19.075Q17.725 20.425 15.9 21.212Q14.075 22 12 22ZM12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12ZM12 20Q15.325 20 17.663 17.663Q20 15.325 20 12Q20 8.675 17.663 6.337Q15.325 4 12 4Q8.675 4 6.338 6.337Q4 8.675 4 12Q4 15.325 6.338 17.663Q8.675 20 12 20Z"/></svg>
-        </button>
+        <div class="card__controls">
+            <button class="button-delete" value="${id}"></button>
+            <button class="check-button ${ state ? "check-button--success": "check-button--disabled"}" value="${id}">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" fill="${ state ? "#FFFFFF" :"#9CA3AF" }"><path xmlns="http://www.w3.org/2000/svg" d="M10.6 16.6 17.65 9.55 16.25 8.15 10.6 13.8 7.75 10.95 6.35 12.35ZM12 22Q9.925 22 8.1 21.212Q6.275 20.425 4.925 19.075Q3.575 17.725 2.788 15.9Q2 14.075 2 12Q2 9.925 2.788 8.1Q3.575 6.275 4.925 4.925Q6.275 3.575 8.1 2.787Q9.925 2 12 2Q14.075 2 15.9 2.787Q17.725 3.575 19.075 4.925Q20.425 6.275 21.212 8.1Q22 9.925 22 12Q22 14.075 21.212 15.9Q20.425 17.725 19.075 19.075Q17.725 20.425 15.9 21.212Q14.075 22 12 22ZM12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12ZM12 20Q15.325 20 17.663 17.663Q20 15.325 20 12Q20 8.675 17.663 6.337Q15.325 4 12 4Q8.675 4 6.338 6.337Q4 8.675 4 12Q4 15.325 6.338 17.663Q8.675 20 12 20Z"/></svg>
+            </button>
+        </div>
     `
-    newTaskCard.children[3].addEventListener('click', deleteTaskById);
-    newTaskCard.children[4].addEventListener('click', setTaskState);
-    console.log(newTaskCard.children[4]);
+    newTaskCard.children[3].children[0].addEventListener('click', deleteTaskById);
+    newTaskCard.children[3].children[1].addEventListener('click', setTaskState);
     
 
     tasksContainer.appendChild(newTaskCard);
@@ -120,7 +121,7 @@ addButton.addEventListener('click', (e)=>{
             text: "La tarea no puede estar vacía",
             duration: 1000,
             position: "center", // `left`, `center` or `right`
-            style: {background: "linear-gradient(to right, #00b09b, #96c93d)",},
+            style: {background: "linear-gradient(to right, #D50505, #E78D0C)",},
             }).showToast();
         console.log('En el else');
     }
---
+++ js/weather.js
@@ -0,0 +1,42 @@
+window.addEventListener('load',()=>{
+
+    console.log('Inicializando scrip de clima...');
+
+    //variables
+    const url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/cordoba?unitGroup=metric&key=8F3GYEZDMZ5EB5WP2966PFYGA&contentType=json";
+    const currentTemp = document.querySelector('.info__temp');
+    const currentLocation = document.querySelector('.info__location');
+    const iconWeather = document.querySelector('#icon-weather');
+    console.log(currentTemp);
+    let responseWeather;
+    const iconsObj = {
+        "cloudy":'/assets/img/cloud.png',
+        "clear-day":''
+    } 
+
+    //funciones
+    // const getInfoWeather =  () => {
+
+    // }
+    const printTemp = () =>{
+        console.log(currentTemp.children[0],currentLocation.children[1]);
+        currentTemp.children[0].innerHTML = `${responseWeather.currentConditions.temp}°`;
+        currentTemp.children[1].innerHTML = `${responseWeather.days[0].tempmax}°/${responseWeather.days[0].tempmin}°`;
+        currentLocation.children[0].innerHTML = responseWeather.resolvedAddress;
+        currentLocation.children[1].innerHTML = `${new Date().toDateString()}`;
+        iconWeather.src=iconsObj[responseWeather.currentConditions.icon];
+    }
+
+    //eventos
+
+    //ejecuciones inmediatas
+    fetch(url)
+        .then( response=>response.json() ) //parseo a objeto literal con .json()
+        .then( (data)=>{
+            console.log(data);
+            responseWeather = data;
+            printTemp();
+        }) //guardo ese parseo en data
+        .catch( error=>console.error(error) )
+
+});
